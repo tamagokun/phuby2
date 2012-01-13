276a435c16c3d9a2bae00ea9fe44b604d726d011
Mike Kruk <mike.kruk@mslgroup.com>
2012-01-13 16:19:13 -0500

    added Module and Object from test project. put some initial info up on the README

---
+++ lib/Phuby/Module.php
@@ -0,0 +1,171 @@
+<?php
+namespace Phuby;
+
+abstract class Module
+{
+	public static $mixins = array();
+	
+	public static function alias($new,$old)
+	{
+		$class = get_called_class();
+		$methods = &$class::methods();
+		$methods[$new] = isset($methods[$old])? $methods[$old] : array(array($class,$old));
+		$methods[$old] = array(array($class,$new));
+	}
+	
+	public static function alias_method($new,$old)
+	{
+		$class = get_called_class();
+		$mixins = &$class::mixins();
+		array_push($mixins["aliases"], array($new,$old));
+		$class::alias($new,$old);
+		$class::update_derived_modules();
+	}
+	
+	public static function alias_method_chain($method,$with)
+	{
+		$class = get_called_class();
+		$class::alias_method("{$method}_without_{$with}",$method);
+		$class::alias_method($method, "{$method}_with_{$with}");
+	}
+	
+	public static function aliases()
+	{
+		$class = get_called_class();
+		$mixins = $class::mixins();
+		return $mixins["aliases"];
+	}
+	
+	public static function ancestors()
+	{
+		$class = get_called_class();
+		$mixins = $class::mixins();
+		return $mixins["ancestors"];
+	}
+	
+	public static function extend($modules)
+	{
+		$modules = (is_array($modules))? $modules : func_get_args();
+		$class = get_called_class();
+		$mixins = &$class::mixins();
+		foreach($modules as $module)
+		{
+			if(!in_array($module, $mixins['ancestors']))
+			{
+				array_unshift($mixins["ancestors"],$module);
+				if(in_array(__CLASS__,class_parents($module)))
+				{
+					foreach( array_reverse($module::ancestors()) as $ancestor)
+						if(!in_array($ancestor, $mixins['ancestors'])) array_unshift($mixins['ancestors'],$ancestor);
+				}
+			}
+		}
+		$class::update_derived_modules();
+	}
+	
+	public static function new_instance($arguments = null)
+	{
+		
+	}
+	
+	public static function new_instance_array($arguments = array())
+	{
+		
+	}
+	
+	public static function &methods()
+	{
+		$class = get_called_class();
+		$mixins = &$class::mixins();
+		if($mixins["methods"]) return $mixins["methods"];
+		$mixins["methods"] = array();
+		$ancestors = array();
+		foreach($mixins["ancestors"] as $ancestor)
+		{
+			if(isset(Module::$mixins[$ancestor]) && Module::$mixins[$ancestor]["methods"])
+			{
+				$mixins["methods"] = (array)Module::$mixins[$ancestor]["methods"];
+				foreach($ancestors as $key=>$module)
+					if(in_array($module,Module::$mixins[$ancestor]["ancestors"])) unset($ancestors[$key]);
+				break;
+			}else
+			{
+				array_unshift($ancestors, $ancestor);
+			}
+		}
+		$methods = get_class_methods($class);
+		foreach($ancestors as $ancestor)
+		{
+			foreach(get_class_methods($ancestor) as $method)
+			{
+				if(!in_array($method,$methods))
+				{
+					if(!isset($mixins["methods"][$method])) $mixins["methods"][$method] = array();
+					array_unshift($mixins["methods"][$method], array($ancestor, $method));
+				}
+			}
+		}
+		foreach($mixins["aliases"] as $alias)
+			Module::alias($class,$alias[0],$alias[1]);
+		return $mixins["methods"];
+	}
+	
+	public static function &mixins()
+	{
+		$class = get_called_class();
+		if(!isset(Module::$mixins[$class]))
+		{
+			$mixins = array('aliases' => array(), 'ancestors' => array($class), 'methods' => false, 'properties' => false);
+			foreach(class_parents($class) as $parent)
+			{
+				if(!in_array($parent,$mixins['ancestors']))
+				{
+					if(in_array(__CLASS__,class_parents($parent)))
+					{
+						$method = __FUNCTION__;
+						$parent_mixins = $parent::$method();
+						$mixins['ancestors'] = array_merge($mixins['ancestors'], $parent_mixins['ancestors']);
+					}else
+					{
+						array_push($mixins['ancestors'], $parent);
+					}
+				}
+			}
+			Module::$mixins[$class] = $mixins;
+		}
+		return Module::$mixins[$class];
+	}
+	
+	public static function &properties()
+	{
+		$class = get_called_class();
+		$mixins = &$class::mixins();
+		if($mixins["properties"]) return $mixins["properties"];
+		
+		$mixins["properties"] = array();
+		$ancestors = $mixins["ancestors"];
+		
+		$properties = get_class_vars("\Phuby\Object");
+		foreach(array_reverse($ancestors) as $ancestor)
+		{
+			foreach(get_class_vars($ancestor) as $property=>$value)
+				if(!isset($properties[$property])) $mixins["properties"][$property] = $value;
+		}
+		return $mixins["properties"];
+	}
+	
+	public static function update_derived_modules()
+	{
+		$class = get_called_class();
+		$ancestors = $class::ancestors();
+		foreach( Module::$mixins as $module => &$mixins)
+		{
+			if($module != $class && in_array($class,$mixins["ancestors"]))
+			{
+				array_splice($mixins["ancestors"], array_search($class,$mixins["ancestors"]), count($mixins["ancestors"]),$ancestors);
+				$mixins["methods"] = false;
+				$mixins["properties"] = false;
+			}
+		}
+	}
+}
---
+++ lib/Phuby/Object.php
@@ -0,0 +1,157 @@
+<?php
+namespace Phuby;
+
+class Object extends Module
+{	
+	public $class;
+	public $instances;
+	public $instance_variables;
+	public $superclass;
+	
+	public function __construct($arguments = array())
+	{
+		$this->class = get_class($this);
+		$this->instances = array();
+		$class = $this->class;
+		$this->instance_variables = $class::properties();
+		$this->superclass = array_pop(class_parents($this->class));
+		if($this->respond_to("initialize"))
+			$this->send_array("initialize",func_get_args());
+	}
+	
+	public function __destruct()
+	{
+		if($this->respond_to("finalize")) $this->send_array("finalize");
+	}
+	
+	public function respond_to($method)
+	{
+		$class = $this->class;
+		$methods = $class::methods();
+		return in_array($method, get_class_methods($this->class)) || (in_array($method, array_keys($methods)) && !empty($methods[$method]));
+	}
+	
+	public function send($method, $arguments=null)
+	{
+		$arguments = func_get_args();
+		return $this->send_array(array_shift($arguments),$arguments);
+	}
+	
+	public function send_array($method,$args=array())
+	{
+		$class = $this->class;
+		if(!$this->respond_to($method)) return null;
+		$methods = $class::methods();
+		if(!isset($methods[$method]) || empty($methods[$method]))
+			return call_user_func_array(array($this,$method), $args);
+		$class = $methods[$method][0][0];
+    $class_method = $methods[$method][0][1];
+		if(!isset($this->instances[$class]) || !$this->instances[$class])
+			$this->instances[$class] = new $class();
+		$this->instances[$class]->inject($this);
+		return call_user_func_array(array($this->instances[$class],$class_method),$args);
+	}
+	
+	public function inject($object)
+	{
+		if($this->is_injected($object)) return true;
+		$class = get_class($object);
+		$this->instances[$class] = $object;
+	}
+		
+	public function is_injected($object)
+	{
+		return array_key_exists(get_class($object),$this->instances);
+	}
+	
+	public function super($arguments=null)
+	{
+		$arguments = func_get_args();
+		$caller = array_pop(array_slice(debug_backtrace(),1,1));
+		if(empty($caller))
+			return false;
+		else
+		{
+			$class = $this->class;
+			$methods = &$class::methods();
+			$aliases = $class::aliases();
+			$method = $caller["function"];
+			foreach(array_reverse($aliases) as $alias)
+			{
+				if($alias[1] == $method)
+				{
+					$method = $alias[0];
+					break;
+				}
+			}
+			if(isset($methods[$method]) && !empty($methods[$method]))
+			{
+				$callee = array_shift($methods[$method]);
+				$result = $this->send_array($method, $arguments);
+				array_unshift($methods[$method], $callee);
+			}else
+			{
+				$result = call_user_func_array("parent::$method",$arguments);
+			}
+		}
+		return $result;
+	}
+	
+	public static function call($prop)
+	{
+		$result = null;
+		foreach(self::$mixins as $name=>$class)
+		{
+			if(isset($name::$$prop))
+			{
+				$result = &$name::$$prop;
+				break;
+			}
+		}
+		return $result;
+	}
+	
+	public function __call($method,$args)
+	{
+		return $this->send_array($method,$args);
+	}
+	
+	public static function __callStatic($method,$args)
+	{
+		$class = get_called_class();
+		foreach($class::mixins() as $class_name=>$module)
+		{
+			if(method_exists($class_name,$method)) return call_user_func_array(array($class_name,$method),$args);
+		}
+	}
+	
+	public function __get($property)
+	{
+		if(isset($this->$property)) return $this->instance_variables[$property];
+		foreach($this->instances as $instance)
+		{
+			if(isset($instance->$property)) return $instance->$property;
+		}
+		return null;
+	}
+	
+	public function __isset($property)
+	{
+		return isset($this->instance_variables[$property]);
+	}
+	
+	public function __set($property,$value)
+	{
+		if(isset($this->$property))
+			return $this->instance_variables[$property] = $value;
+		foreach($this->instances as $instance)
+		{
+			if(isset($instance->$property)) $instance->$property = $value;
+		}
+	}
+	
+	public function __unset($property)
+	{
+		unset($this->instance_variables[$property]);
+	}
+}
